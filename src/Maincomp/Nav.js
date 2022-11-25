67b1061a2477802d5c2727f190c2fd05b4cb93d8
SUHAS N H <suhas4497@gmail.com>
2022-11-25 20:17:36 +0000

    Update 3 files

--- src/Maincomp/Nav.js
+++ src/Maincomp/Nav.js
@@ -3,20 +3,32 @@ import logo from "./img/logo.svg";
 import profile from "./img/profile.svg";
 import history from "./img/history.svg";
 import wallet from "./img/wallet.svg";
+import meta from "./img/meta.svg";
 import "./Home.css";
 
 export default function Nav1() {
   return (
-    <div className="connect">
-      <div className="nav-item1">
+    <div className="navWrapper">
+      <div className="nav-left">
         <img src={logo} alt="Logo" />
       </div>
-      <div className="nav-item2">
-        <img src={history} alt="histroy" />
 
-        <img src={profile} alt="profile" />
-
-        <img src={wallet} alt="wallet" />
+      <div className="nav-right">
+        <div className="nav-item">
+          <img src={history} alt="histroy" />
+        </div>
+        <div className="nav-item">
+          <img src={profile} alt="profile" />
+        </div>
+        <div className="nav-item">
+          <img src={wallet} alt="wallet" />
+        </div>
+        <div className="nav-item1">
+          <div className="nav-sub">
+            <img src={meta} alt="metamask" />
+          </div>
+          <div className="nav-sub">0x99....9A9</div>
+        </div>
       </div>
     </div>
   );
